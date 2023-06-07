8387c4cf9afa79553bc4759facaf418ecf5b0e54
toriomara <budkevich.vladimir@gmail.com>
2023-06-07 19:21:46 +0300

    feat: 15th project, cors added

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const helmet = require('helmet');
 const { errors } = require('celebrate');
+const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const rateLimit = require('express-rate-limit');
 const { errorMiddleware } = require('./middlewares/errorMiddleware');
@@ -22,6 +23,7 @@ const app = express();
 
 app.use(limiter);
 app.use(helmet());
+app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
