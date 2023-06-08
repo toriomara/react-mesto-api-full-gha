358688241c477683c0beb2b460022efa45c905cc
toriomara <budkevich.vladimir@gmail.com>
2023-06-08 17:14:26 +0300

    feat: 15th project, lint correct

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -2,16 +2,16 @@ const whitelist = [
   'http://localhost:3000',
   'https://toriomara.nomoredomains.rocks',
   'http://toriomara.nomoredomains.rocks',
-]
+];
 
 const corsOptions = {
-  origin: function (origin, callback) {
+  origin(origin, callback) {
     if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true)
+      callback(null, true);
     } else {
-      callback(new Error('Not allowed by CORS'))
+      callback(new Error('Not allowed by CORS'));
     }
-  }
-}
+  },
+};
 
-module.exports = {corsOptions}
+module.exports = { corsOptions };
